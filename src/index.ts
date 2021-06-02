dadaa0a7c69a7a148c96961d901da3420dd8c3e2
Anderson Oliveira <tandersonf@hotmail.com>
2021-06-02 17:48:45 -0300

    criar class, student e teacher feito, com bug no result

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import express, { Express, Request, Response } from "express"
 import cors from "cors"
 import { AddressInfo } from "net"
+import connection from "./connection";
 
-
+export type Class = {
+name:string,
+start_date:Date,
+end_date:Date,
+module: number
+}
 
 const app: Express = express();
 app.use(express.json());
@@ -19,3 +25,51 @@ const server = app.listen(process.env.PORT || 3003, () => {
 });
 
 app.get('/', (req, res) => { res.send('Ping Pong!!!') })
+
+app.get('/teachers',(req:Request,res:Response) => {
+   try {
+      
+   } catch (error) {
+      console.log(error)
+      res.send(error.message || error.sqlMessage)
+   }
+})
+
+app.post('/teacher',async (req:Request,res:Response):Promise<void> => {
+   try {
+      const {name,email,birth_date,class_id} = req.body
+      const result = await connection.raw(`
+      INSERT  INTO teacher (name,email,birth_date,class_id) 
+      VALUES ("${name}","${email}","${birth_date}","${class_id}");`)
+      res.status(200).send(result)
+   } catch (error) {
+      console.log(error)
+      res.send(error.message || error.sqlMessage)
+   }
+})
+
+app.post('/student',async (req:Request,res:Response):Promise<void> => {
+   try {
+      const {name,email,birth_date,class_id} = req.body
+      const result = await connection.raw(`
+      INSERT INTO student (name,email,birth_date,class_id) 
+      VALUES ("${name}","${email}","${birth_date}","${class_id}");`)
+      res.status(200).send(result[1])
+   } catch (error) {
+      console.log(error)
+      res.send(error.message || error.sqlMessage)
+   }
+})
+
+app.post('/class',async (req:Request,res:Response):Promise<void> => {
+   try {
+      const {name,start_date,end_date,module} = req.body
+      const result = await connection.raw(`
+      INSERT  INTO class (name,start_date,end_date,module) 
+      VALUES ("${name}","${start_date}","${end_date}","${module}");`)
+      res.status(200).send(result)
+   } catch (error) {
+      console.log(error)
+      res.send(error.message || error.sqlMessage)
+   }
+})
