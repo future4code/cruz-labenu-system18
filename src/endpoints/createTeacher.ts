c6c13fc11da21a3e24b00eec7cfcce1db50876ac
Adryane Fernandes <edryfernandes@gmail.com>
2021-06-03 16:20:59 -0300

    tratamento de erro do createTeacher

--- src/endpoints/createTeacher.ts
+++ src/endpoints/createTeacher.ts
@@ -1,15 +1,37 @@
 import { Request, Response } from "express";
 import connection from "../connection";
 
-export default async function (req:Request,res:Response):Promise<void>  {
-    try {
-       const {name,email,birth_date,class_id} = req.body
-       const result = await connection.raw(`
-       INSERT  INTO teacher (name,email,birth_date,class_id) 
-       VALUES ("${name}","${email}","${birth_date}","${class_id}");`)
-       res.status(200).send(result)
-    } catch (error) {
-       console.log(error)
-       res.send(error.message || error.sqlMessage)
+async function createTeacher(req: Request, res: Response): Promise<void> {
+  try {
+    const { name, email, birthDate, classId } = req.body;
+
+    if (!name || !email || !birthDate || !classId) {
+      res.statusCode = 400;
+      throw new Error("incomplete or invalid data");
     }
- }
+
+    await connection.raw(`
+      INSERT  INTO teacher (name,email,birth_date,class_id) 
+      VALUES (
+        "${name}",
+        "${email}",
+        "${birthDate.split("/").reverse().join("-")}",
+        "${classId}"
+      );
+    `);
+
+    res.status(201).send({ message: "created" });
+  } catch (error) {
+    if (error.sqlMessage && error.sqlMessage.includes("Duplicate")) {
+      res.status(400).send({ message: "email has already been registered" });
+    }
+    if (error.sqlMessage && error.sqlMessage.includes("Incorrect date value")) {
+      res.status(400).send({ message: "incorrect date format" });
+    }
+
+    console.error(error);
+    res.send({ message: error.message || error.sqlMessage });
+  }
+}
+
+export default createTeacher;
