8b7426cedd82ad74622cd3f4d16b775774619cd5
Adryane Fernandes <edryfernandes@gmail.com>
2021-06-03 16:06:59 -0300

    tratamento de erro do createStudant

--- src/endpoints/createStudent.ts
+++ src/endpoints/createStudent.ts
@@ -1,17 +1,40 @@
 import { Request, Response } from "express";
 import connection from "../connection";
 
+export default async function createStudent(
+  req: Request,
+  res: Response
+): Promise<void> {
+  try {
+    const { name, email, birthDate, classId } = req.body;
 
-
-export default async function createStudent(req:Request,res:Response):Promise<void>  {
-    try {
-       const {name,email,birth_date,class_id} = req.body
-       const result = await connection.raw(`
-       INSERT INTO student (name,email,birth_date,class_id) 
-       VALUES ("${name}","${email}","${birth_date}","${class_id}");`)
-       res.status(200).send(result[1])
-    } catch (error) {
-       console.log(error)
-       res.send(error.message || error.sqlMessage)
+    if (!name || !email || !birthDate || !classId) {
+      res.statusCode = 400;
+      throw new Error("incomplete or invalid data");
     }
- }
+
+    await connection.raw(`
+         INSERT INTO student (name,email,birth_date,class_id)
+         VALUES (
+            "${name}",
+            "${email}",
+            "${birthDate.split("/").reverse().join("-")}",
+            "${classId}"
+         );
+    `);
+
+    res.status(201).send({ message: "created" });
+  } catch (error) {
+    if (error.sqlMessage.includes("Duplicate")) {
+      res.statusCode = 400
+      res.send({ message: "email has already been registered" });
+    }
+    if (error.sqlMessage.includes("Incorrect date value")) {
+      res.statusCode = 400
+      res.send({ message: "incorrect date format" });
+    }
+
+    console.log(error);
+    res.send({ message: error.message || error.sqlMessage });
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,12 @@ import cors from "cors"
 import { AddressInfo } from "net"
 import deleteStudents from "./endpoints/deleteStudents";
 import connection from "./connection";
-import  getAllClasses  from "./endpoints/getAllClasses";
+import getAllClasses  from "./endpoints/getAllClasses";
 import getAllTeachers from "./endpoints/getAllTeachers";
 import getAllStudents from "./endpoints/getAllStudents";
 import seeStudentHobbie from "./endpoints/seeStudentHobbie";
 import createClass from "./endpoints/createClass";
+import createStudent from './endpoints/createStudent'
 
 
 export type Class = {
@@ -22,16 +23,9 @@ app.use(express.json());
 app.use(cors())
 
 
-app.delete("/student/:id", deleteStudents)
 
-const server = app.listen(process.env.PORT || 3003, () => {
-   if (server) {
-      const address = server.address() as AddressInfo;
-      console.log(`Server is running in http://localhost:${address.port}`);
-   } else {
-      console.error(`Failure upon starting server.`);
-   }
-});
+
+
 
 app.get('/', async (req, res) => { res.send('Ping Pong!!!') })
 
@@ -57,17 +51,17 @@ app.post('/teacher',async (req:Request,res:Response):Promise<void> => {
    }
 })
 
-app.post('/student',async (req:Request,res:Response):Promise<void> => {
-   try {
-      const {name,email,birth_date,class_id} = req.body
-      const result = await connection.raw(`
-      INSERT INTO student (name,email,birth_date,class_id) 
-      VALUES ("${name}","${email}","${birth_date}","${class_id}");`)
-      res.status(200).send(result[1])
-   } catch (error) {
-      console.log(error)
-      res.send(error.message || error.sqlMessage)
-   }
-})
+app.post('/student', createStudent)
 
 app.post('/class',createClass)
+
+app.delete("/student/:id", deleteStudents)
+
+const server = app.listen(process.env.PORT || 3003, () => {
+   if (server) {
+      const address = server.address() as AddressInfo;
+      console.log(`Server is running in http://localhost:${address.port}`);
+   } else {
+      console.error(`Failure upon starting server.`);
+   }
+});
