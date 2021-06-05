876df931585c5ac88e33da4bde9c0b86cedb36b7
Anderson Oliveira <tandersonf@hotmail.com>
2021-06-05 18:02:44 -0300

    readicionado server, removido por engano

--- src/index.ts
+++ src/index.ts
@@ -32,6 +32,17 @@ app.use(express.json());
 app.use(cors());
 
 
+
+const server = app.listen(process.env.PORT || 3003, () => {
+  if (server) {
+    const address = server.address() as AddressInfo;
+    console.log(`Server is running in http://localhost:${address.port}`);
+  } else {
+    console.error(`Failure upon starting server.`);
+  }
+});
+
+
 app.get("/class", getAllClasses);
 
 app.get("/teacher", getAllTeachers);
